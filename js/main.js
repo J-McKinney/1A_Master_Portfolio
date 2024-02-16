eb51fce7174e3dd6ecab207d05f2dbed72cacf6d
Jesse McKinney <jessemckinney513@gmail.com>
2024-02-16 12:37:55 -0500

    khg

--- js/main.js
+++ js/main.js
@@ -10,12 +10,10 @@ jQuery(document).ready(function($) {
         "async": true,
         "crossDomain": true,
         // "url": "https://cors-anywhere.herokuapp.com/https://express.etrade.com/phx/rtao/init/bank",
-        "url": "https://cors.bridged.cc/https://express.etrade.com/phx/rtao/init/bank",
-        // "url": "https://corsanywhere.herokuapp.com/https://express.etrade.com/phx/rtao/init/bank",
+        // "url": "https://cors.bridged.cc/https://express.etrade.com/phx/rtao/init/bank",
+        "url": "https://corsanywhere.herokuapp.com/https://express.etrade.com/phx/rtao/init/bank",
         "method": "POST",
         "headers": {
-          "Accept": "*/*",
-          "User-Agent": "Thunder Client (https://www.thunderclient.com)",
           "Content-Type": "application/json"
         },
         "processData": false,
